9f6e4d5036b2d25a442b99124fa9eb906d966a3e
Aleksandar <102791994+aleksa012po@users.noreply.github.com>
2023-02-05 21:10:33 +0100

    Add files via upload

---
+++ C_Arduino_RTC_and_EEPROM/main.c
@@ -0,0 +1,27 @@
+/*
+ * C_Arduino_RTC_and_EEPROM.c
+ *
+ * Created: 23.1.2023. 09:08:12
+ * Author : Aleksandar Bogdanovic
+ */ 
+
+#include <avr/io.h>
+#include "src/config/config.h"
+#include <util/delay.h>
+#include "src/rtc/rtc.h"
+#include "src/rtc/rtc_define.h"
+#include "src/twi/twi.h"
+#include "src/eeprom/eeprom.h"
+#include "src/eeprom/eeprom_define.h"
+
+int main(void)
+{
+	RTC_setup();
+	_delay_ms(EEPROM_DELAY);
+	EEPROM_setup();
+	
+    while (1) 
+    {
+    }
+}
+
---
+++ C_Arduino_RTC_and_EEPROM/src/rtc/config.h
@@ -0,0 +1,13 @@
+/*
+ * config.h
+ *
+ * Created: 19.1.2023. 00:16:20
+ *  Author: Aleksandar Bogdanovic
+ */ 
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define F_CPU 16000000UL
+
+#endif /* CONFIG_H_ */
